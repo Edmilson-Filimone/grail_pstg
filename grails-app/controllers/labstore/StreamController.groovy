253b5c29fecfd4e280e385cad6273502d9374cc2
Edmilson-Filimone <75740869+Edmilson-Filimone@users.noreply.github.com>
2022-08-04 15:29:42 +0200

    Add files via upload

--- grails-app/controllers/labstore/StreamController.groovy
+++ grails-app/controllers/labstore/StreamController.groovy
@@ -1,39 +1,52 @@
 package labstore
 
 import labstore.Material
-import com.itextpdf.kernel.pdf.*;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
+import labstore.Entry
+import labstore.Outs
 
 
 class StreamController {
 
-    def index() { 
-
-        File file = new File("hello.pdf") 
-        PdfDocument pdf = new PdfDocument(new PdfWriter(file));
-        Document document = new Document(pdf)
-        String line = "Hello Edmilson Filimone! Welcome to iTextPdf"
-        String line2 = "Custou mas valeu a pena, aqui esta um pdf em java++, kkkk Groovy :)"
-        document.add(new Paragraph(line))
-        document.add(new Paragraph(line2))
-        document.close()
-        //response.contentType  = 'application/pdf'
-        response.outputStream << file.getBytes()
-        //response.outputStream.flush()
-    }
-
-    def MyCsv(){
+    def material_csv(){
         def matList = Material.list()
         def data = new ArrayList()
-        data.add(Arrays.asList("Material", "Quantidade"))
+        data.add(Arrays.asList("Material", "Quantidade", "Expire Date"))
 
         matList.each {
-            data.add(Arrays.asList(it.name, it.quantity.toString()))
+            data.add(Arrays.asList(it.name, it.quantity.toString(), it.expireDate))
         }
-        def file = File.createTempFile('mycsv', '.csv')
+
+        def file = File.createTempFile('Material', '.csv')
         file.text = data*.join(',').join(System.lineSeparator())
         response.contentType  = 'text/csv'
         response.outputStream << file.getBytes()
     }
+
+    def entry_csv(){
+        def matList = Entry.list()
+        def data = new ArrayList()
+        data.add(Arrays.asList("Material", "Quantidade", "Entry Date"))
+
+        matList.each {
+            data.add(Arrays.asList(it.material.name, it.quantity.toString(), it.date))
+        }
+        def file = File.createTempFile('Entradas', '.csv')
+        file.text = data*.join(',').join(System.lineSeparator())
+        response.contentType  = 'text/csv'
+        response.outputStream << file.getBytes()
+    }
+
+    def outs_csv(){
+        def matList = Outs.list()
+        def data = new ArrayList()
+        data.add(Arrays.asList("Material", "Quantidade","Destiny", "Out Date"))
+
+    matList.each {
+            data.add(Arrays.asList(it.material.name, it.quantity.toString(),it.destiny ,it.date.toString()))
+        }
+    def file = File.createTempFile('Outs', '.csv')
+        file.text = data*.join(',').join(System.lineSeparator())
+        response.contentType  = 'text/csv'
+        response.outputStream << file.getBytes()
+    }  
 }
